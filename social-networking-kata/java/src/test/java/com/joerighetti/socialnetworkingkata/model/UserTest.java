f8c2dcba726d575fa06399513d5214d69e8d6b02
righetti66 <righetti66@gmail.com>
2021-04-01 11:06:11 -0400

    push

---
+++ social-networking-kata/java/src/test/java/com/joerighetti/socialnetworkingkata/model/UserTest.java
@@ -0,0 +1,34 @@
+package com.joerighetti.socialnetworkingkata.model;
+
+import org.junit.jupiter.api.Test;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+
+
+
+    @Test
+    void getUserId() {
+    assertEquals(1, user.getUserId());
+    
+    }
+
+    @Test
+    void setUserId() {
+
+    }
+
+    @Test
+    void getUsername() {
+
+    }
+
+    @Test
+    void setUsername() {
+
+    }
+}
