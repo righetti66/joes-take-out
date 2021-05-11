bf623c0c7b5d57021bf7909ba9157d3ee0989bb4
righetti66 <84022212+righetti66@users.noreply.github.com>
2021-05-11 11:27:35 -0400

    heroku

--- take-out-app/server.js
+++ take-out-app/server.js
@@ -1,17 +1,16 @@
-
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<joes-take-out-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<joes-take-out-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.use(requireHTTPS);
+app.use(express.static('./dist/<take-out-app.json>'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/<take-out-app.json>/'}
+  );
+  });
+  app.listen(process.env.PORT || 8080);
